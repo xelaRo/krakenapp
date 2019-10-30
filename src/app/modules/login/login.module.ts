5555ae52820d1c4f691c236dfbad9530ffd01122
Alex <aldea.alex19@gmail.com>
2019-10-30 17:12:43 +0200

    second

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations/animations';
 import { MaterialDesignModule } from 'src/app/shared/material-design/material-design.module';
+import { RegisterRoutingModule } from '../register/register-routing.module';
 
 @NgModule({
   declarations: [
--- src/app/modules/register/register.module.ts
+++ src/app/modules/register/register.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RegisterComponent } from './register.component';
 import { RegisterRoutingModule } from './register-routing.module';
 import { MaterialDesignModule } from 'src/app/shared/material-design/material-design.module';
 import { ReactiveFormsModule } from '@angular/forms';
